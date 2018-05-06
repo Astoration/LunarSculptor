1eeeec16f7f612adb6a3d33d3d2e071ea2a1583c
Astoration <ybdh00@naver.com>
2018-05-06 18:18:04 +0900

    유니티용 싱글톤 템플릿 추가

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 유니티용 싱글톤 템플릿
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+    private static T _instance;
+
+    public static T instance{
+        get{
+            if (!_instance)
+            {
+                var gameObject = new GameObject();
+                var component = gameObject.AddComponent<T>();
+                _instance = component;
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(!instance){
+            _instance = GetComponent<T>();
+        }
+    }
+}
