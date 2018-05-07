1ea1dbaae2e1955f0e7d57778259d0ae32023033
Astoration <ybdh00@naver.com>
2018-05-08 08:17:29 +0900

    일단은 마감

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -13,7 +13,7 @@ public class CharacterInfo
     internal int hp = 3;
     public float defenceCoolDown = 1f;
     public float currentDef = 1f;
-    public float lunarGague = 0f;
+    public float lunarGague = 100f;
 }
 
 public class CharacterControl : Singleton<CharacterControl> {
@@ -22,17 +22,18 @@ public class CharacterControl : Singleton<CharacterControl> {
 
     [Header("캐릭터 조작 정보")]
     public CharacterInfo info = new CharacterInfo();
-    public ReactiveProperty<bool> isGrounded = new ReactiveProperty<bool>(true);
+    public ReactiveProperty<bool> isGrounded = new ReactiveProperty<bool>(false);
+    public GameObject skillObject;
     private ObservableUpdateTrigger trigger;
     private int atkType = 1;
     private double AttackCoolTime = 0.1f;
     private bool isAtkReady = true;
     private float attackRange = 10f;
-    private readonly int GroundLayer = 1<<8;
+    private readonly int GroundLayer = 8;
     private readonly int DestructibleLayer = 9;
 
     // Awake는 스크립트 인스턴스가 로드되는 중에 호출됩니다.
-    private void Awake()
+    protected override void Awake()
     {
         initialTrigger();
         initialComponent();
@@ -64,17 +65,6 @@ public class CharacterControl : Singleton<CharacterControl> {
     private void setupStateStream()
     {
         trigger.UpdateAsObservable()
-              .Select(_ => Mathf.RoundToInt(transform.position.y))
-              .DistinctUntilChanged()
-              .ThrottleFrame(5)
-               .Where(_ => rigid.IsTouchingLayers(GroundLayer))
-               .Subscribe(y =>
-               {
-                   rigid.simulated = false;
-                   isGrounded.Value = true;
-               });
-
-        trigger.UpdateAsObservable()
                .Select(_ => info)
                .Subscribe(info => GameManager.instance.updateCharacterUI(info));
         trigger.UpdateAsObservable()
@@ -103,14 +93,11 @@ public class CharacterControl : Singleton<CharacterControl> {
         var input = CrossPlatformInputManager.instance;
         input.jump
              .Where(jump => jump)// 점프가 눌렸을때
-             .Select(jump => rigid.velocity.y) // 물리량의 y를 확인해서
-             .Where(y => Mathf.Abs(y) < 0.01f) // 물리량이 없을때만
+             .Where(_ => isGrounded.Value)
              .Select(y => rigid)
              .Subscribe(rigid =>
              {
-                 rigid.AddForce(Vector2.up * info.jumpHeight);
-                 rigid.simulated = true;
-                 isGrounded.Value = false;
+                 jump();
              }); //점프
         input.attack
              .Where(atk => atk)
@@ -122,6 +109,26 @@ public class CharacterControl : Singleton<CharacterControl> {
              .Where(defence => defence)
              .Where(defence => info.defenceCoolDown<=info.currentDef)
              .Subscribe(def => defence());
+        input.skill
+             .Where(skill => skill)
+             .Where(skill => 100f <= info.lunarGague)
+             .Subscribe(skill => activeSkill());
+    }
+
+    private void activeSkill()
+    {
+        Instantiate(skillObject, transform.position, Quaternion.identity);
+        info.lunarGague = 0f;
+    }
+
+    private void jump()
+    {
+        if (GameManager.instance.destricbles.Count == 0) return;
+        var item = GameManager.instance.destricbles.Peek();
+        if (Vector2.Distance(item.transform.position, transform.position)<5f) return;
+        rigid.simulated = true;
+        isGrounded.Value = false;
+        rigid.AddForce(Vector2.up * info.jumpHeight);
     }
 
     private void defence()
@@ -161,5 +168,14 @@ public class CharacterControl : Singleton<CharacterControl> {
     {
         animator = GetComponentInChildren<Animator>();
         rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == GroundLayer)
+        {
+            isGrounded.Value = true;
+            rigid.simulated = false;
+        }
     }
 }
---
+++ Assets/Scripts/SkillObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillObject : MonoBehaviour {
+    public float speed = 5f;
+    public int destricbleLayer = 9;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.up * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != destricbleLayer) return;
+        var districble = collision.gameObject.GetComponent<Destructible>();
+        districble.Damage(999);
+    }
+
+
+}
