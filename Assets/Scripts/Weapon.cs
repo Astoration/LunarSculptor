69e22f4c042c6148465915b3e04a473faef9fc13
Astoration <ybdh00@naver.com>
2018-05-08 06:46:46 +0900

    무기 데이터 추가

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Weapon",menuName ="WeaponData")]
+public class Weapon : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite image;
+    public int damge;
+    public float damageRatio;
+    public float lunarBoost;
+}
