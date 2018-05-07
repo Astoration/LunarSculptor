7a9957e39b0b3269eeb44ad97503d7ed5e385878
Astoration <ybdh00@naver.com>
2018-05-07 18:33:10 +0900

    스프라이트 풀링 메서드 추가

--- Assets/Scripts/SpritePool.cs
+++ Assets/Scripts/SpritePool.cs
@@ -34,6 +34,29 @@ public class SpritePool : MonoBehaviour {
         return result;
     }
 
+    public static GameObject InstantiatePool(string name, Vector3 position, Quaternion rotation)
+    {
+        Stack<GameObject> stack = null;
+        if (pools.ContainsKey(name)) stack = pools[name] as Stack<GameObject>;
+        var item = PoolObjectDB.instance.hashTable[name] as GameObject;
+        if (stack == null || stack.Count == 0)
+        {
+            if (stack == null)
+            {
+                stack = new Stack<GameObject>();
+                pools.Add(name, stack);
+            }
+            var gameObject = Instantiate(item, position,rotation);
+            gameObject.name = name;
+            return gameObject;
+        }
+        var result = stack.Pop();
+        result.SetActive(true);
+        result.transform.position = position;
+        result.transform.rotation = rotation;
+        return result;
+    }
+
     public void DestroyPool(){
         this.gameObject.SetActive(false);
         var stack = pools[name] as Stack<GameObject>;
